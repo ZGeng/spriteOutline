541283f544dabd0115047f283eecf7b4c6e47869
Z G <gzblake@gmail.com>
2017-03-14 22:25:05 -0800

    directly use render target and property block
    In sprite outline simple, use renderimage and copy property block to
    prevent gilch when create new texture

--- Assets/Scripts/SpriteOutlineSimple.cs
+++ Assets/Scripts/SpriteOutlineSimple.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class SpriteOutline : MonoBehaviour
+public class SpriteOutlineSimple : MonoBehaviour
 {
     //set a trigger 
     [SerializeField] bool showOutline = false;
     [SerializeField] Material outline_mat;
     [SerializeField] int outlineSortingOrder;
 
-    [Header("Sprites Generated")]
-    [SerializeField] int spriteCount = 0;
+    [Header("Texture Generated")]
+    [SerializeField] int textureCount = 0;
     SpriteRenderer spriteRenderer;
     GameObject oSprite;
     SpriteRenderer outlineRenderer;
+    MaterialPropertyBlock outlineProperties;
+    Texture destTexture;
     
 
     void Start()
     {   
-        
         spriteRenderer = GetComponent<SpriteRenderer>();
-
+        outlineProperties = new MaterialPropertyBlock();
         //create outline object
         createOutlineGameObject();
     }
@@ -31,14 +32,12 @@ public class SpriteOutline : MonoBehaviour
     void LateUpdate()
     {
         Sprite sourceSprite = spriteRenderer.sprite;
-        if (sourceSprite.name.Substring(0, 7) != "outline")
-        {
             //check if the sprite already in the list 
-            if (!SpriteStorage.spriteDict.ContainsKey(sourceSprite))
+            if (!SpriteStorage.textureDict.ContainsKey(sourceSprite.texture))
             {
                 //do the generation of the new sprite 
                 GenerateSprite(sourceSprite);
-                spriteCount++;
+                textureCount++;
             }
 
             if (showOutline)
@@ -48,9 +47,16 @@ public class SpriteOutline : MonoBehaviour
                 {
                     //set the object active 
                     oSprite.SetActive(true);
+                    //get and set the property 
+                    
                     outlineRenderer.sortingOrder = outlineSortingOrder;
                 }
-                outlineRenderer.sprite = SpriteStorage.spriteDict[sourceSprite];
+                //using property block to change the outline texture
+                outlineRenderer.sprite = sourceSprite;
+                destTexture = SpriteStorage.textureDict[spriteRenderer.sprite.texture];
+                spriteRenderer.GetPropertyBlock(outlineProperties);
+                outlineProperties.SetTexture("_MainTex", destTexture);
+                outlineRenderer.SetPropertyBlock(outlineProperties);
             }
             else
             {
@@ -59,9 +65,6 @@ public class SpriteOutline : MonoBehaviour
                     oSprite.SetActive(false);
                 }
             }
-
-        }
-
     }
 
 
@@ -73,31 +76,10 @@ public class SpriteOutline : MonoBehaviour
         if (!SpriteStorage.textureDict.ContainsKey(sourceTexture))
         {
             RenderTexture tempRenderTexture = new RenderTexture(sourceTexture.width, sourceTexture.height, 1);
-
-
             RenderTexture originalRenderTexture = RenderTexture.active;
-            Texture2D destTexture2D = new Texture2D(tempRenderTexture.width, tempRenderTexture.height, TextureFormat.RGBA32, false);
-
-            RenderTexture.active = tempRenderTexture;
             Graphics.Blit(sourceTexture, tempRenderTexture, outline_mat);
-            destTexture2D.ReadPixels(new Rect(0, 0, tempRenderTexture.width, tempRenderTexture.height), 0, 0);
-            destTexture2D.Apply();
-            //put the destTexture2D into the dictionary
-            SpriteStorage.textureDict.Add(sourceTexture, destTexture2D);
-            //set back the rendertexture
-            RenderTexture.active = originalRenderTexture;
+            SpriteStorage.textureDict.Add(sourceTexture, tempRenderTexture);
         }
-
-        //generate the sprite 
-        Rect sRect = sourceSprite.rect;
-        float sPixels = sourceSprite.pixelsPerUnit;
-        RenderTexture original = RenderTexture.active;
-        Texture2D destTexture = (Texture2D)SpriteStorage.textureDict[sourceTexture];
-
-        Sprite tempSp = Sprite.Create(destTexture, sRect, new Vector2(0.5f, 0.5f), sPixels);
-        tempSp.name = "outline_" + sourceSprite.name;
-        //add the sprite to the dict
-        SpriteStorage.spriteDict.Add(sourceSprite, tempSp);
     }
 
     void createOutlineGameObject()
@@ -108,6 +90,5 @@ public class SpriteOutline : MonoBehaviour
         outlineRenderer = oSprite.AddComponent<SpriteRenderer>();
         outlineRenderer.sortingOrder = outlineSortingOrder;
     }
-
 
 }
--- Assets/Scripts/SpriteStorage.cs
+++ Assets/Scripts/SpriteStorage.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class SpriteStorage{
 
     public static Dictionary<Sprite, Sprite> spriteDict = new Dictionary<Sprite, Sprite>();
-    public static Dictionary<Texture2D, Texture2D> textureDict = new Dictionary<Texture2D, Texture2D>();
+    public static Dictionary<Texture, Texture> textureDict = new Dictionary<Texture, Texture>();
 
 }
